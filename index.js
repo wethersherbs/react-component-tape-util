d2d0fc13cc988c7bc5a1474ac53f28dc9e9d76ba
Tom Harding <tomjharding@live.co.uk>
2016-07-22 11:25:59 +0100

    Making index.js the Entry Point
    NPM got a bit confused with the compiled and uncompiled versions.
    Updated the package.json file to use the right version, and made
    that index.js just in case it threw another tantrum.

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-import tape from 'tape'
-import addAssertions from 'extend-tape'
-import jsxEquals from 'tape-jsx-equals'
-import { createRenderer } from 'react-addons-test-utils'
+'use strict';
 
-export default addAssertions(tape, { jsxEquals })
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.renderJsx = undefined;
 
-export const renderJsx = component => {
-  const renderer = createRenderer()
-  renderer.render(component)
-  return renderer.getRenderOutput()
-}
+var _tape = require('tape');
+
+var _tape2 = _interopRequireDefault(_tape);
+
+var _extendTape = require('extend-tape');
+
+var _extendTape2 = _interopRequireDefault(_extendTape);
+
+var _tapeJsxEquals = require('tape-jsx-equals');
+
+var _tapeJsxEquals2 = _interopRequireDefault(_tapeJsxEquals);
+
+var _reactAddonsTestUtils = require('react-addons-test-utils');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = (0, _extendTape2.default)(_tape2.default, { jsxEquals: _tapeJsxEquals2.default });
+const renderJsx = exports.renderJsx = component => {
+  const renderer = (0, _reactAddonsTestUtils.createRenderer)();
+  renderer.render(component);
+  return renderer.getRenderOutput();
+};
+
